77293092cd13f79fd9d918993984d257e2a2c412
Savva <wot-sb@yandex.ru>
2020-03-19 17:28:28 +0400

    final version

--- Parser+Generics/src/expression/generic/GenericTabulator.java
+++ Parser+Generics/src/expression/generic/GenericTabulator.java
@@ -8,7 +8,7 @@ import expression.parser.ExpressionParser;
 import java.util.Map;
 
 public class GenericTabulator implements Tabulator {
-    private static final Map<String, Operation<?>> MODES = Map.of(
+    private static final Map<String, Operation> MODES = Map.of(
             "i", new IntegerOperation(true),
             "d", new DoubleOperation(),
             "bi", new BigIntegerOperation(),
@@ -19,30 +19,29 @@ public class GenericTabulator implements Tabulator {
 
     @Override
     public Object[][][] tabulate(String mode, String expression, int x1, int x2, int y1, int y2, int z1, int z2) {
-        return makeTable(MODES.get(mode), expression, x1, x2, y1, y2, z1, z2);
+        return makeTable(getOperation(mode), expression, x1, x2, y1, y2, z1, z2);
     }
 
-    private static Operation get(String mode) {
+    private Operation<?> getOperation(final String mode) {
         if (!MODES.containsKey(mode)) {
-            throw new ComputationalException(mode + " not supported");
+            throw new ComputationalException("No mode " + mode);
         }
         return MODES.get(mode);
     }
 
-    private <T> Object[][][] makeTable(Operation<T> operator, String expression, int x1, int x2, int y1, int y2, int z1, int z2) {
-        ExpressionParser<T> parser = new ExpressionParser<T>(operator);
-        TripleExpression<T> tripleExpression = parser.parse(expression);
+    private <T> Object[][][] makeTable(Operation<T> op, String expression, int x1, int x2, int y1, int y2, int z1, int z2) {
+        ExpressionParser<T> parser = new ExpressionParser<>(op);
+        TripleExpression<T> commonExpression = parser.parse(expression);
         Object[][][] ans = new Object[x2 - x1 + 1][y2 - y1 + 1][z2 - z1 + 1];
-        for (int x = 0; x <= x2 - x1; x++) {
-            for (int y = 0; y <= y2 - y1; y++) {
-                for (int z = 0; z <= z2 - z1; z++) {
+        for (int x = x1; x <= x2; x++) {
+            for (int y = y1; y <= y2; y++) {
+                for (int z = z1; z <= z2; z++) {
                     try {
-                        ans[x][y][z] = tripleExpression.evaluate(
-                                operator.parse(Integer.toString(x + x1)),
-                                operator.parse(Integer.toString(y + y1)),
-                                operator.parse(Integer.toString(z + z1)));
+                        ans[x - x1][y - y1][z - z1] = commonExpression.evaluate(op.parse(Integer.toString(x)),
+                                op.parse(Integer.toString(y)),
+                                op.parse(Integer.toString(z)));
                     } catch (ComputationalException e) {
-                        ans[x][y][z] = null;
+                        ans[x - x1][y - y1][z - z1] = null;
                     }
                 }
             }
