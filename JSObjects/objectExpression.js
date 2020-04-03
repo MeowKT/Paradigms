19eaec6735e03b73860a20dffbcaf95902f3f893
Savva <wot-sb@yandex.ru>
2020-04-03 17:43:00 +0400

    korn change

--- JSObjects/objectExpression.js
+++ JSObjects/objectExpression.js
@@ -1,124 +1,122 @@
 "use strict";
 
+function expressionInterface(constr, evaluate, toString, diff) {
+    constr.prototype.evaluate = evaluate;
+    constr.prototype.toString = toString;
+    constr.prototype.diff = diff;
+}
+
 function Const(x) {
     this.x = x;
 }
-Const.prototype.evaluate = function (expr) { return this.x; };
-Const.prototype.toString = function () { return this.x.toString(); };
-Const.prototype.diff = function (variable) { return new Const(0); };
+expressionInterface(
+    Const,
+    function() { return parseInt(this.x) },
+    function() { return this.x.toString() },
+    function() { return ZERO }
+);
 
+const ZERO = new Const(0);
 const ONE = new Const(1);
 const TWO = new Const(2);
-
-const variables = {
-    "x": 0,
-    "y": 1,
-    "z": 2
-};
 
 function Variable(variable) {
     this.variable = variable;
     this.number = variables[variable];
 }
+expressionInterface(
+    Variable,
+    function(...vars) { return vars[this.number] },
+    function() { return this.variable },
+    function(variable) { return this.variable === variable ? ONE : ZERO }
+);
 
-Variable.prototype.evaluate = function(...elements) { return elements[this.number]; };
-Variable.prototype.toString = function() { return this.variable.toString(); };
-Variable.prototype.diff = function(variable) { return new Const(variable === this.variable ? 1 : 0); };
+const variables = {
+    x: 0,
+    y: 1,
+    z: 2
+};
 
-function MultiExpression(op, symbol, diffRule, ...args) {
+function MultiExpression(...args) {
     this.args = args;
-    this.op = op;
-    this.symbol = symbol;
-    this.diffRule = diffRule;
 }
-MultiExpression.prototype.toString = function() {
-    return this.args.slice().reduce((sum, current) => sum + current.toString() + " ", "") + this.symbol;
-};
-MultiExpression.prototype.evaluate = function(...elements) {
-    return this.op(...this.args.map(el => el.evaluate(...elements)));
-};
-MultiExpression.prototype.diff = function(variable) {
-    return this.diffRule(variable, ...this.args);
-};
+expressionInterface(
+    MultiExpression,
+    function(...elements) { return this.op(...this.args.map(el => el.evaluate(...elements))); },
+    function() { return this.args.join(" ") + " " + this.symbol; },
+    function(variable) { return this.diffRule(variable, ...this.args); }
+);
 
-function Negate(expr) {
-    MultiExpression.call(this,
-        a => -a,
-        "negate",
-        (name, a) =>
-            new Negate(a.diff(name)),
-        expr);
+function makeOperation(op, symbol, diffRule) {
+    let operation = function (...args) {
+        MultiExpression.call(this, ...args);
+    };
+    operation.prototype = Object.create(MultiExpression.prototype);
+    operation.prototype.op = op;
+    operation.prototype.symbol = symbol;
+    operation.prototype.diffRule = diffRule;
+    return operation;
 }
-Negate.prototype = Object.create(MultiExpression.prototype);
 
-function Add(left, right) {
-    MultiExpression.call(this,
-        (a, b) => a + b,
-        "+",
-        (name, a, b) =>
-            new Add(a.diff(name), b.diff(name)),
-        left, right);
-}
-Add.prototype = Object.create(MultiExpression.prototype);
+const Negate = makeOperation(
+    a => -a,
+    "negate",
+    (name, a) => new Negate(a.diff(name))
+);
 
-function Subtract(left, right) {
-    MultiExpression.call(this,
-        (a, b) => a - b,
-        "-",
-        (name, a, b) =>
-            new Subtract(a.diff(name), b.diff(name)),
-        left, right);
-}
-Subtract.prototype = Object.create(MultiExpression.prototype);
+const Add = makeOperation(
+    (a, b) => a + b,
+    "+",
+    (name, a, b) => new Add(a.diff(name), b.diff(name)));
 
-function Multiply(left, right) {
-    MultiExpression.call(this,
-        (a, b) => a * b, "*",
-        (name, a, b) =>
-            new Add(
-                new Multiply(a, b.diff(name)),
-                new Multiply(a.diff(name), b)),
-        left, right);
-}
-Multiply.prototype = Object.create(MultiExpression.prototype);
+const Subtract = makeOperation(
+    (a, b) => a - b,
+    "-",
+    (name, a, b) => new Subtract(a.diff(name), b.diff(name)),
+);
 
-function Divide(left, right) {
-    MultiExpression.call(this,
-        (a, b) => a / b,
-        "/",
-        (name, a, b) =>
-            new Divide(
-                new Subtract(
-                    new Multiply(a.diff(name), b),
-                    new Multiply(a, b.diff(name))),
-                new Multiply(b, b)),
-        left, right);
-}
-Divide.prototype = Object.create(MultiExpression.prototype);
+const Multiply = makeOperation(
+    (a, b) => a * b,
+    "*",
+    (name, a, b) =>
+        new Add(
+            new Multiply(a, b.diff(name)),
+            new Multiply(a.diff(name), b))
+);
 
-function Gauss(a, b, c, x) {
-    MultiExpression.call(this,
-        (a, b, c, x) => a * Math.exp(-(x - b) * (x - b) / (c * c * 2)),
-        "gauss",
-        (name, a, b, c, x) =>
-            new Multiply(
-                new Gauss(ONE, b, c, x),
-                new Add(
-                    a.diff(name),
-                    new Multiply(
-                        a,
-                        new Negate(
-                            new Divide(
-                                new Multiply(
-                                    new Subtract(x, b),
-                                    new Subtract(x, b)),
-                                new Multiply(
-                                    TWO,
-                                    new Multiply(c, c)))).diff(name)))),
+const Divide = makeOperation(
+    (a, b) => a / b,
+    "/",
+    (name, a, b) =>
+        new Divide(
+            new Subtract(
+                new Multiply(a.diff(name), b),
+                new Multiply(a, b.diff(name))),
+            new Multiply(b, b))
+);
 
-        a, b, c, x);
-}
-Gauss.prototype = Object.create(MultiExpression.prototype);
+const Gauss = makeOperation(
+    (a, b, c, x) => a * Math.exp(-(x - b) * (x - b) / (c * c * 2)),
+    "gauss",
+    function(name, a, b, c, x) {
+        let sub = new Subtract(x, b);
+        return new Multiply(
+                    new Gauss(ONE, b, c, x),
+                    new Add(
+                        a.diff(name),
+                        new Multiply(
+                            a,
+                            new Negate(
+                                new Divide(
+                                    new Multiply(
+                                        sub,
+                                        sub),
+                                    new Multiply(
+                                        TWO,
+                                        new Multiply(c, c)))).diff(name))))
+    }
+);
+
 
 const operations = {
     "gauss": [Gauss, 4],
@@ -129,20 +127,18 @@ const operations = {
     "negate": [Negate, 1],
 };
 
-const parse = expression => {
+function parse(expression) {
     let stack = [];
-    function parseElement(element) {
-        if (element in operations) {
-            let operation = operations[element][0];
-            let cntArgs = operations[element][1];
+
+    for (let token of expression.trim().split(/\s+/)) {
+        if (token in operations) {
+            let [operation, cntArgs] = operations[token];
             stack.push(new operation(...stack.splice(-cntArgs)));
-        } else if (element in variables) {
-            stack.push(new Variable(element));
+        } else if (token in variables) {
+            stack.push(new Variable(token));
         } else {
-            stack.push(new Const(parseInt(element)));
+            stack.push(new Const(parseInt(token)));
         }
     }
-
-    expression.trim().split(/\s+/).forEach(parseElement);
     return stack.pop();
-};
+}
